68a395f6c271a795ace46eccc80673d9402c828d
Jeet <makanijeet51@gmail.com>
2026-06-19 11:10:24 +0530

    added database models

--- myuserapp/models.py
+++ myuserapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField("Name",max_length=30)
+    mobile = models.CharField("Number",max_length=10)
+    email = models.EmailField("Email",blank=False)
+
+    def __str__(self):
+        return self.name
